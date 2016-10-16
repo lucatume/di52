e45316170d1dae525e1a2e37fb414ee10394ba51
Luca Tumedei <luca@theaveragedev.com>
2016-10-16 12:36:21 +0200

    debug version

--- tests/functional/ArrayAccessBindingTest.php
+++ tests/functional/ArrayAccessBindingTest.php
@@ -42,6 +42,8 @@ class ArrayAccessBindingTest extends PHPUnit_Framework_TestCase
      */
     public function it_should_allow_binding_an_a_singleton_callback_to_an_interface()
     {
+        $this->asssertEquals('5.2.17', phpversion());
+        return;
         if (version_compare(phpversion(), '5.3.0') < 0) {
             $this->markTestSkipped();
         }
