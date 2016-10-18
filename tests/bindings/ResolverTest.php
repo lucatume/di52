babb6496f17833172aed30c6c44e6149e71f2075
Luca Tumedei <luca@theaveragedev.com>
2016-10-18 10:58:21 +0200

    updated tests to 3.6 exclusion rules

--- tests/bindings/ResolverTest.php
+++ tests/bindings/ResolverTest.php
@@ -36,56 +36,6 @@ class ResolverTest extends PHPUnit_Framework_TestCase
         $resolver->bind('ConcreteClassImplementingTestInterfaceOne', 'ConcreteClassImplementingTestInterfaceTwo');
     }
 
-    /**
-     * @test
-     * it should allow binding a callback to an interface
-     */
-    public function it_should_allow_binding_a_callback_to_an_interface()
-    {
-        if (!version_compare(phpversion(), '5.2.17', '>')) {
-            $this->markTestSkipped();
-        }
-
-        $resolver = $this->makeInstance();
-
-        $object = (object)array('foo' => 'bar');
-        $callback = function () use ($object) {
-            return $object;
-        };
-
-        $resolver->bind('TestInterfaceOne', $callback);
-
-        $out = $resolver->resolve('TestInterfaceOne');
-        $this->assertEquals('bar', $out->foo);
-
-        $object->foo = 'baz';
-
-        $out = $resolver->resolve('TestInterfaceOne');
-        $this->assertEquals('baz', $out->foo);
-    }
-
-    /**
-     * @test
-     * it should rerun the callback on each resolution
-     */
-    public function it_should_rerun_the_callback_on_each_resolution()
-    {
-        if (!version_compare(phpversion(), '5.2.17', '>')) {
-            $this->markTestSkipped();
-        }
-
-        $resolver = $this->makeInstance();
-
-        $callback = function () {
-            return microtime();
-        };
-
-        $resolver->bind('TestInterfaceOne', $callback);
-        $one = $resolver->resolve('TestInterfaceOne');
-        $two = $resolver->resolve('TestInterfaceOne');
-
-        $this->assertNotEquals($one, $two);
-    }
 
     /**
      * @test
@@ -281,45 +231,6 @@ class ResolverTest extends PHPUnit_Framework_TestCase
         $this->assertSame($outOne, $outTwo);
     }
 
-    /**
-     * @test
-     * it should allow binding a singleton callback to an interface
-     */
-    public function it_should_allow_binding_a_singleton_callback_to_an_interface()
-    {
-        if (!version_compare(phpversion(), '5.2.17', '>')) {
-            $this->markTestSkipped();
-        }
-
-        $sut = $this->makeInstance();
-        $sut->singleton('TestInterfaceOne', function () {
-            return microtime();
-        });
-        $outOne = $sut->resolve('TestInterfaceOne');
-        $outTwo = $sut->resolve('TestInterfaceOne');
-
-        $this->assertSame($outOne, $outTwo);
-    }
-
-    /**
-     * @test
-     * it should allow binding a singleton callback to a class
-     */
-    public function it_should_allow_binding_a_singleton_callback_to_a_class()
-    {
-        if (!version_compare(phpversion(), '5.2.17', '>')) {
-            $this->markTestSkipped();
-        }
-
-        $sut = $this->makeInstance();
-        $sut->singleton('ConcreteClassImplementingTestInterfaceOne', function () {
-            return microtime();
-        });
-        $outOne = $sut->resolve('ConcreteClassImplementingTestInterfaceOne');
-        $outTwo = $sut->resolve('ConcreteClassImplementingTestInterfaceOne');
-
-        $this->assertSame($outOne, $outTwo);
-    }
 
     /**
      * @test
@@ -482,55 +393,6 @@ class ResolverTest extends PHPUnit_Framework_TestCase
         $this->assertSame($instanceOne, $instanceTwo);
     }
 
-    /**
-     * @test
-     * it should resolve singleton bindings of different interfaces with same implementation to same callback
-     */
-    public function it_should_resolve_singleton_bindings_of_different_interfaces_with_same_implementation_to_same_callback()
-    {
-        if (!version_compare(phpversion(), '5.2.17', '>')) {
-            $this->markTestSkipped();
-        }
-
-        $container = $this->makeInstance();
-
-        $f = function () {
-            return microtime();
-        };
-
-        $container->singleton('TestInterfaceOne', $f);
-        $container->singleton('TestInterfaceTwo', $f);
-
-        $outputOne = $container->resolve('TestInterfaceOne');
-        $outputTwo = $container->resolve('TestInterfaceTwo');
-
-        $this->assertSame($outputOne, $outputTwo);
-    }
-
-    /**
-     * @test
-     * it should resolve singleton bindings of different class and interface with same implementation to same callback
-     */
-    public function it_should_resolve_singleton_bindings_of_different_class_and_interface_with_same_implementation_to_same_callback()
-    {
-        if (!version_compare(phpversion(), '5.2.17', '>')) {
-            $this->markTestSkipped();
-        }
-
-        $container = $this->makeInstance();
-
-        $f = function () {
-            return microtime();
-        };
-
-        $container->singleton('TestInterfaceOne', $f);
-        $container->singleton('ClassOne', $f);
-
-        $outputOne = $container->resolve('TestInterfaceOne');
-        $outputTwo = $container->resolve('ClassOne');
-
-        $this->assertSame($outputOne, $outputTwo);
-    }
 
     /**
      * @test
@@ -701,56 +563,6 @@ class ResolverTest extends PHPUnit_Framework_TestCase
         $sut->singleton('c.one', new ClassOne());
 
         $this->assertSame($sut->resolve('c.one'), $sut->resolve('c.one'));
-    }
-
-    /**
-     * @test
-     * it should allow binding callbacks by slug
-     */
-    public function it_should_allow_binding_callbacks_by_slug()
-    {
-        if (!version_compare(phpversion(), '5.2.17', '>')) {
-            $this->markTestSkipped();
-        }
-
-        $sut = $this->makeInstance();
-
-        $sut->bind('c.one', function () {
-            return new ClassOne();
-        });
-        $sut->bind('c.base', function () {
-            return new BaseClass();
-        });
-
-        $this->assertInstanceOf('ClassOne', $sut->resolve('c.one'));
-        $this->assertNotSame($sut->resolve('c.one'), $sut->resolve('c.one'));
-        $this->assertInstanceOf('BaseClass', $sut->resolve('c.base'));
-        $this->assertNotSame($sut->resolve('c.base'), $sut->resolve('c.base'));
-    }
-
-    /**
-     * @test
-     * it should allow binding callbacks as singletons by slug
-     */
-    public function it_should_allow_binding_callbacks_as_singletons_by_slug()
-    {
-        if (!version_compare(phpversion(), '5.2.17', '>')) {
-            $this->markTestSkipped();
-        }
-
-        $sut = $this->makeInstance();
-
-        $sut->singleton('c.one', function () {
-            return new ClassOne();
-        });
-        $sut->singleton('c.base', function () {
-            return new BaseClass();
-        });
-
-        $this->assertInstanceOf('ClassOne', $sut->resolve('c.one'));
-        $this->assertSame($sut->resolve('c.one'), $sut->resolve('c.one'));
-        $this->assertInstanceOf('BaseClass', $sut->resolve('c.base'));
-        $this->assertSame($sut->resolve('c.base'), $sut->resolve('c.base'));
     }
 
     /**
