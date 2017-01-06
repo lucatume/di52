e1f1ce0c327d1ba90e4374c21ad283ea0d9d2feb
Luca Tumedei <luca@theaveragedev.com>
2017-01-06 21:38:35 +0100

    update to use autoload on php 5.3 >

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,9 @@
 <?php
-require_once dirname(__DIR__) . '/vendor/autoload_52.php';
+if (version_compare(PHP_VERSION, '5.3.0', '<')) {
+    require_once dirname(__DIR__) . '/vendor/autoload_52.php';
+} else {
+    require_once dirname(__DIR__) . '/vendor/autoload.php';
+}
 include_once dirname(__FILE__) . '/data/test-classes.php';
 include_once dirname(__FILE__) . '/data/namespaced-test-classes.php';
 include_once dirname(__FILE__) . '/data/test-providers.php';
