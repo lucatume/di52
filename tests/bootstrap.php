a5f656987441475f8005a2f61cdce55a162cd380
Luca Tumedei <luca@theaveragedev.com>
2017-01-06 21:43:12 +0100

    restored php 52 autoload file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,5 @@
 <?php
-if (version_compare(PHP_VERSION, '5.3.0', '<')) {
-    require_once dirname(__DIR__) . '/vendor/autoload_52.php';
-} else {
-    require_once dirname(__DIR__) . '/vendor/autoload.php';
-}
-include_once dirname(__FILE__) . '/data/test-classes.php';
-include_once dirname(__FILE__) . '/data/namespaced-test-classes.php';
-include_once dirname(__FILE__) . '/data/test-providers.php';
+require_once dirname(__DIR__) . '/vendor/autoload_52.php';
+require_once dirname(__FILE__) . '/data/test-classes.php';
+require_once dirname(__FILE__) . '/data/namespaced-test-classes.php';
+require_once dirname(__FILE__) . '/data/test-providers.php';
