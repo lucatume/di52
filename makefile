ec1622dac7d71f7109caec5e3d1550ea23ccb65c
Luca Tumedei <luca@theaveragedev.com>
2017-01-16 23:28:10 +0100

    updated code for speed

--- makefile
+++ makefile
@@ -1,4 +1,5 @@
 test:
+	/Applications/Mamp/bin/php/php5.2.17/bin/php vendor/bin/phpunit-php52
 	vendor/bin/phpunit-php52
 
 cover:
---
+++ src/tad/DI52/closuresSupport.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Builds and returns a closure to be used to build to lazily make objects on PHP 5.3+.
+ *
+ * @param tad_DI52_Container $container
+ * @param string $classOrInterface
+ * @param string $method
+ *
+ * @return Closure
+ */
+function di52_callbackClosure(tad_DI52_Container $container, $classOrInterface, $method) {
+	return function () use ($container, $classOrInterface, $method) {
+		$a = func_get_args();
+		$i = $container->make($classOrInterface);
+		return call_user_func_array(array($i, $method), $a);
+	};
+}
