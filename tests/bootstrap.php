94477ca3b0af2f8a263cb76c8837a81fb91f0b2d
Lucas Bustamante <lucasfbustamante@gmail.com>
2020-10-26 13:00:59 -0300

    Remove car-classes.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,6 @@
 <?php
 require_once dirname(__FILE__) . '/../vendor/autoload.php';
 require_once dirname(__FILE__) . '/data/test-classes.php';
-require_once dirname(__FILE__) . '/data/car-classes.php';
 if (version_compare(PHP_VERSION, '5.3.0', '>=')) {
     require_once dirname(__FILE__) . '/data/namespaced-test-classes.php';
 }
