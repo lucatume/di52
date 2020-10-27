4fe83d8f04fe7e42c5e0a4d011db32383a8a0994
Luca Tumedei <luca@theaveragedev.com>
2020-10-27 13:59:47 +0100

    test(bootstrap.php) use correct autoload file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,11 @@
 <?php
-require_once dirname(dirname(__FILE__)) . '/vendor/autoload_52.php';
+$autoloadFile = dirname(dirname(__FILE__)) . '/vendor/autoload_52.php';
+
+if(!is_file($autoloadFile)){
+	$autoloadFile = dirname(dirname(__FILE__)) . '/vendor/autoload.php';
+}
+require_once $autoloadFile;
+
 require_once dirname(__FILE__) . '/data/test-classes.php';
 if (version_compare(PHP_VERSION, '5.3.0', '>=')) {
     require_once dirname(__FILE__) . '/data/namespaced-test-classes.php';
