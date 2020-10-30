7fffaf30fccde3bb98403b0cac9fceb5b941b9a3
Lucas Bustamante <lucasfbustamante@gmail.com>
2020-10-30 14:27:00 -0300

    PHP 5.2 don't have ::class

--- tests/52-compat/Container52CompatTest.php
+++ tests/52-compat/Container52CompatTest.php
@@ -1497,7 +1497,7 @@ class Container52CompatTest extends PHPUnit_Framework_TestCase {
 	public function should_throw_correct_exception_when_injecting_missing_class_in_the_constructor() {
 		$container = new tad_DI52_Container();
 		try {
-			$container->make( Car::class );
+			$container->make( 'Car' );
 		} catch ( Exception $e ) {
 			assertMatchesSnapshots( $e->getMessage() );
 		}
@@ -1506,10 +1506,10 @@ class Container52CompatTest extends PHPUnit_Framework_TestCase {
 	/** @test */
 	public function should_throw_correct_exception_when_injecting_missing_class_in_the_constructor_and_nested_dependency_singleton() {
 		$container = new tad_DI52_Container();
-		$container->bind( Car::class );
-		$container->singleton( Engine::class );
+		$container->bind( 'Car' );
+		$container->singleton( 'Engine' );
 		try {
-			$container->make( Car::class );
+			$container->make( 'Car' );
 		} catch ( Exception $e ) {
 			assertMatchesSnapshots( $e->getMessage() );
 		}
@@ -1518,9 +1518,9 @@ class Container52CompatTest extends PHPUnit_Framework_TestCase {
 	/** @test */
 	public function should_throw_correct_exception_when_injecting_missing_class_in_the_constructor_2() {
 		$container = new tad_DI52_Container();
-		$container->singleton( Engine::class );
+		$container->singleton( 'Engine' );
 		try {
-			$container->make( Engine::class );
+			$container->make( 'Engine' );
 		} catch ( Exception $e ) {
 			assertMatchesSnapshots( $e->getMessage() );
 		}
@@ -1530,7 +1530,7 @@ class Container52CompatTest extends PHPUnit_Framework_TestCase {
 	public function should_throw_correct_exception_when_injecting_class_with_private_constructor() {
 		$container = new tad_DI52_Container();
 		try {
-			$container->make( LowerEngine::class );
+			$container->make( 'LowerEngine' );
 		} catch ( Exception $e ) {
 			assertMatchesSnapshots( $e->getMessage() );
 		}
@@ -1540,7 +1540,7 @@ class Container52CompatTest extends PHPUnit_Framework_TestCase {
 	public function should_throw_correct_exception_when_making_class_with_private_constructor_as_a_dependency() {
 		$container = new tad_DI52_Container();
 		try {
-			$container->make( Clutch::class );
+			$container->make( 'Clutch' );
 		} catch ( Exception $e ) {
 			assertMatchesSnapshots( $e->getMessage() );
 		}
@@ -1550,7 +1550,7 @@ class Container52CompatTest extends PHPUnit_Framework_TestCase {
 	public function should_throw_correct_exception_when_making_class_with_invalid_class_as_dependency() {
 		$container = new tad_DI52_Container();
 		try {
-			$container->make( Valve::class );
+			$container->make( 'Valve' );
 		} catch ( Exception $e ) {
 			assertMatchesSnapshots( $e->getMessage() );
 		}
