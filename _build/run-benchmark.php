4c6fec539f2a860f2b7e5a11bda2045666eb8687
Luca Tumedei <luca@theaveragedev.com>
2020-12-11 13:32:56 +0100

    build(run-benchmark.php) refine output of test suite/case runner

--- _build/run-benchmark.php
+++ _build/run-benchmark.php
@@ -25,4 +25,12 @@ list( $testSuiteNumber, $testCaseNumber ) = explode( '.', $argv[1] );
 
 $testRunner = new TestRunner();
 
-echo $testRunner->runTest((int)$testSuiteNumber, (int)$testCaseNumber, 'di52')->toJson();
+$json =  $testRunner->runTest((int)$testSuiteNumber, (int)$testCaseNumber, 'di52')->toJson();
+
+if(($decoded = json_decode($json,true)) === false){
+	echo "Failed to decode test suite result: "	 . json_last_error_msg();
+	return;
+}
+list( $time, $memory ) = array_values( $decoded );
+printf( 'Test suite %d.%d: time: %fms, memory %fMB', $testSuiteNumber, $testCaseNumber, $time, $memory );
+
