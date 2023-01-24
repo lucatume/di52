db3dbb3ca49128cfd0c9f22b30a9d54635eeef97
Justin Frydman <justin.frydman@gmail.com>
2023-01-24 13:53:06 -0700

    Add enum parameter tests

---
+++ tests/Builders/parameter-test-enum-classes.php
@@ -0,0 +1,22 @@
+<?php
+
+enum TestBackedEnum: string {
+    case DRAFT = 'draft';
+    case PUBLISHED = 'published';
+    case ARCHIVED = 'archived';
+}
+
+class ClassWithEnumDependency
+{
+    public function __construct(
+        private readonly TestBackedEnum $status
+    ) {}
+}
+
+class UnionTypeEnumClass {
+
+    public function __construct(
+        private readonly TestBackedEnum|string $status
+    ) {}
+
+}
